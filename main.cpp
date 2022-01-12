457ae7367989ef312970a6d73dc0d8d0f2fecd66
MateoProgramador <mramirezv1@est.ups.edu.ec>
2022-01-11 22:46:53 -0500

    Calculo de salarios TERMINADO

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "salarios.h"
-
+#include "obrero.h"
 #include <QApplication>
-
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Salarios w;
     w.show();
+
+    Obrero *objObrero1 = new Obrero ("Juan",47,TipoJornada::Vespertina);
+
+
+
+
+
     return a.exec();
 }
--- obrero.cpp
+++ obrero.cpp
@@ -2,5 +2,42 @@
 
 Obrero::Obrero(QObject *parent) : QObject(parent)
 {
-
+    this->m_nombre = "";
+    this->m_horas = 0;
+    this->m_jornada = TipoJornada::Matutina;
 }
+
+const QString &Obrero::nombre() const
+{
+    return m_nombre;
+}
+
+void Obrero::setNombre(const QString &newNombre)
+{
+    m_nombre = newNombre;
+}
+
+int Obrero::horas() const
+{
+    return m_horas;
+}
+
+void Obrero::setHoras(int newHoras)
+{
+    m_horas = newHoras;
+}
+
+TipoJornada Obrero::jornada() const
+{
+    return m_jornada;
+}
+
+void Obrero::setJornada(TipoJornada newJornada)
+{
+    m_jornada = newJornada;
+}
+
+Obrero::Obrero(const QString &nombre, int horas, TipoJornada jornada) : m_nombre(nombre),
+    m_horas(horas),
+    m_jornada(jornada)
+{}
