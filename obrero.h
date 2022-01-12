62bd20f8f66464dc6a5ce5e13e019baff371604e
MateoProgramador <mramirezv1@est.ups.edu.ec>
2022-01-11 20:46:53 -0500

    Terminado el diseño

---
+++ obrero.h
@@ -0,0 +1,16 @@
+#ifndef OBRERO_H
+#define OBRERO_H
+
+#include <QObject>
+
+class Obrero : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Obrero(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // OBRERO_H
